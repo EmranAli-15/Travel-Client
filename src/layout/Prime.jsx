e4e7a592f5fa409ac10f5842d1edb2a05649fd59
EmranAli-15 <mohammademranali15@gmail.com>
2023-08-28 01:42:29 +0600

    working on navbar

--- src/layout/Prime.jsx
+++ src/layout/Prime.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from '../pages/shared/footer/Footer';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../pages/shared/navbar/Navbar';
 
 const Prime = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
---
+++ src/pages/shared/navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import logo from '../../../assets/home/logo.png'
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div className='hidden md:block'>
+            <div className="flex items-center justify-between navbar bg-base-100">
+                <div>
+                    <Link to="/">
+                        <img src={logo} alt="" />
+                    </Link>
+                </div>
+                <div>
+                    central
+                </div>
+                <div className="flex-none">
+                    <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                            </div>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li>
+                                <a className="justify-between">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </a>
+                            </li>
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
