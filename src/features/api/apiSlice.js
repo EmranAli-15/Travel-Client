cac92b0fd52165db3f5eef542d03eee431a31476
EmranAli-15 <mohammademranali15@gmail.com>
2023-09-24 00:00:56 +0600

    .

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://travel-server-mu.vercel.app/",
+        baseUrl: "https://travel-server-mu.vercel.app",
         prepareHeaders: (headers, { getState }) => {
             const token = localStorage.getItem('access-token')
             if (token) {
